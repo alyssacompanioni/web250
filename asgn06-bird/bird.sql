5cbdc5774a25b30416959dd19d40943f13913f31
alyssa <alyssa.companioni@gmail.com>
2025-10-08 19:58:45 -0400

    changes db_credentials for asgn06-bird

--- asgn06-bird/bird.sql
+++ asgn06-bird/bird.sql
@@ -20,7 +20,7 @@ CREATE TABLE `birds` (
 --
 
 INSERT INTO `birds` (`id`, `common_name`, `habitat`, `food`, `conservation_id`, `backyard_tips`) VALUES
-(8, 'Carolina Wren`', 'Open woodlands', 'Insects', 1, 'Carolina Wrens visit suet-filled feeders during winter.'),
+(8, 'Carolina Wren', 'Open woodlands', 'Insects', 1, 'Carolina Wrens visit suet-filled feeders during winter.'),
 (9, 'Tufted Titmouse', 'Forests', 'Insects', 1, 'Tufted Titmouse are regulars at backyard bird feeders, especially in winter. They prefer sunflower seeds but will eat suet, peanuts, and other seeds as well.'),
 (10, 'Ruby-Throated Hummingbird', 'Open woodlands', 'Nectar', 1, 'You can attract Ruby-throated Hummingbirds to your backyard by setting up hummingbird feeders or by planting tubular flowers.'),
 (11, 'Eastern Towhee', 'Scrub', 'Omnivore', 1, 'Eastern Towhees are likely to visit – or perhaps live in – your yard if you’ve got brushy, shrubby, or overgrown borders.'),
--- asgn06-bird/private/db_credentials.php
+++ asgn06-bird/private/db_credentials.php
@@ -1,8 +1,8 @@
 <?php 
 
 define("DB_SERVER", "localhost");
-define("DB_USER", "bird_user");
-define("DB_PASS", "BirdPass");
-define("DB_NAME", "bird");
+define("DB_USER", "uxyaopmbbarls");
+define("DB_PASS", "bird_user1234");
+define("DB_NAME", "dbzuvafpyyc48l");
 
 ?>
