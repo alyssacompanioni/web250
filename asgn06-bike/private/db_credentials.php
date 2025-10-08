9f47dcb4dda4595a04173729c6fd32bcc9f625e3
alyssa <alyssa.companioni@gmail.com>
2025-10-08 19:49:13 -0400

    cleans up extra files from asgn06-bike

--- asgn06-bike/private/db_credentials.php
+++ asgn06-bike/private/db_credentials.php
@@ -5,16 +5,13 @@
 // 2. Unique credentials on development and production servers
 // 3. Unique credentials if working with multiple developers
 
-//localhost credentials
-// define("DB_SERVER", "localhost");
-// define("DB_USER", "chain_gang_user");
-// define("DB_PASS", "P@ssword1");
-// define("DB_NAME", "chain_gang");
+// localhost credentials
+define("DB_SERVER", "localhost");
+define("DB_USER", "chain_gang_user");
+define("DB_PASS", "P@ssword1");
+define("DB_NAME", "chain_gang");
 
-define('DB_SERVER', 'localhost');
-define('DB_USER', 'umy8becxgkrxf');
-define('DB_PASS', 'P@ssword-1234');
-define('DB_NAME', 'db4tr7crgwbcis');
-
-//test
-?>
+// define('DB_SERVER', 'localhost');
+// define('DB_USER', 'umy8becxgkrxf');
+// define('DB_PASS', 'P@ssword-1234');
+// define('DB_NAME', 'db4tr7crgwbcis');
