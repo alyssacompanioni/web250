fb6ec6b759ff24ecd38d4edb92d3c4feeedadc28
alyssa <alyssa.companioni@gmail.com>
2025-10-28 10:59:07 -0400

    adds asgn08-bird folder with asgn07 files

---
+++ asgn08-bird/private/db_credentials.php
@@ -0,0 +1,14 @@
+<?php 
+
+// localhost credentials
+define("DB_SERVER", "localhost");
+define("DB_USER", "bird_user");
+define("DB_PASS", "bird_user1234");
+define("DB_NAME", "bird");
+
+// define("DB_SERVER", "localhost");
+// define("DB_USER", "uxyaopmbbarls");
+// define("DB_PASS", "bird_user1234");
+// define("DB_NAME", "dbzuvafpyyc48l");
+
+?>
---
+++ asgn08-bird/public/birds.php
@@ -0,0 +1,37 @@
+<?php
+require_once('../private/initialize.php');
+$page_title = 'Sightings';
+?> 
+<?php include(SHARED_PATH . '/public_header.php'); ?>
+
+<h2>Bird inventory</h2>
+<p>This is a short list -- start your birding!</p>
+
+<table id="inventory">
+  <tr>
+    <th>Common Name</th>
+    <th>Habitat</th>
+    <th>Food</th>
+    <th>Conservation Level</th>
+    <th>Backyard Tips</th>
+    <th>&nbsp;</th>
+  </tr>
+
+  <?php
+  $birds = Bird::find_all();
+  
+  foreach($birds as $bird) {
+  ?>
+
+    <tr>
+      <td><?php echo h($bird->common_name); ?></td>
+      <td><?php echo h($bird->habitat); ?></td>
+      <td><?php echo h($bird->food); ?></td>
+      <td><?php echo h($bird->get_conservation()); ?></td>
+      <td><?php echo h($bird->backyard_tips); ?></td>
+      <td><a href="detail.php?id=<?php echo h($bird->id); ?>">View</td>
+    </tr>
+  <?php } ?>
+</table>
+
+<?php include(SHARED_PATH . '/public_footer.php'); ?>
